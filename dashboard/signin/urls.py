cd5140e7c36395115a7acb5c10ce2e794430fef0
b3lon9 <b3lon9@gmail.com>
2022-12-02 07:07:39 +0900

    + detail
    [수정내용]
    - 기본적인 틀 구성, 세세한 로직은 구역을 나누어 작성할 예정

--- dashboard/signin/urls.py
+++ dashboard/signin/urls.py
@@ -3,5 +3,7 @@ from signin import views
 
 app_name = "signin"
 urlpatterns = [
-    path("", views.login_page, name="login_page")
+    path("", views.login_page, name="login_page"),
+    path("<int:user_id>/", views.detail),
+    path("user_create", views.create, name='create'),
 ]
